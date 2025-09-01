0b9a564c1d8b0087dd3991caad0790c0dfee32a1
Bruno Fabre <bruno.hfabre@gmail.com>
2025-09-01 14:31:47 -0300

    init dark theme

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -1,9 +1,11 @@
-import { Text, View } from 'react-native'
+import { SafeAreaView, Text, View } from 'react-native'
 
 export default function Home() {
   return (
-    <View className="flex-1 bg-red-100">
-      <Text>Home screen</Text>
-    </View>
+    <SafeAreaView className="flex-1 bg-neutral-50 dark:bg-neutral-900">
+      <View className="flex-1 p-6">
+        <Text className="text-neutral-950 dark:text-white">Home screen</Text>
+      </View>
+    </SafeAreaView>
   )
 }
