c9da71379063c1eab873ad905b2de1d825785910
Bruno Fabre <bruno.hfabre@gmail.com>
2025-09-04 17:29:10 -0300

    add theme colors

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,19 @@
+import colors from 'tailwindcss/colors'
+
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: ['./src/**/*.{ts,tsx}'],
   presets: [require('nativewind/preset')],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        gray: colors.neutral,
+        primary: {
+          50: 'black',
+          100: 'white',
+        },
+      },
+    },
   },
   plugins: [],
 }
